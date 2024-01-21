c51b205621ebabe5c04ffd7ee9c2bcd17326d1f9
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2024-01-22 00:24:28 +0100

    update configs

--- setup-all.sh
+++ setup-all.sh
@@ -1,3 +1,4 @@
-cd nimbora_yield_l2 && setup.sh
-cd nimbora_yield_l1 && 
+cd nimbora-yield-l1-public && setup.sh && cd ..
+cd nimbora-yield-l1-public && cp config.json nimbora-yield-l2-public/l1-config.json
+cd nimbora-yield-l2-public && setup.sh
 cd nimbora-relayer-public && docker-compose up -d && cd ..
